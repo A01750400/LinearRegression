609fc96e4f81909841285e27900cd95f21ef5ac8
Javier Moreno <73722244+A01750400@users.noreply.github.com>
2022-09-04 23:44:15 -0500

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -7,10 +7,17 @@ def gradient_descent(curr_m, curr_b, data, alfa):
     for i in range(n):
         x = data.iloc[i].X
         y = data.iloc[i].Y
-
         m_gradient += -(2/n) * x * (y - (curr_m * x + curr_b))
         b_gradient += -(2/n) * (y - (curr_m * x + curr_b))
-
     m = curr_m - m_gradient * alfa
     b = curr_b - b_gradient * alfa
     return m, b
+
+
+def loss(m, b, data):
+    error = 0
+    for i in range(len(data)):
+        x = data.iloc[i].X
+        y = data.iloc[i].Y
+        error += (y - (m * x + b)) ** 2
+    return error / float(len(data))
