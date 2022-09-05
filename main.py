c48f7fea140c078bc66f6111424eb0475675913c
Javier Moreno <73722244+A01750400@users.noreply.github.com>
2022-09-04 21:03:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from linear_regression import gradient_descent
 
-data = pd.read_csv("slr02.csv")
-print(data)
+if __name__ == "__main__":
+    data = pd.read_csv("slr02.csv")
+    print(data)
 
-m = 0
-b = 0
-alfa = 0.001
-epochs = 10
+    m = 0
+    b = 0
+    alfa = 0.001
+    epochs = 10
 
-for i in range(epochs):
-    m,b = gradient_descent(m, b, data, alfa)
+    for i in range(epochs):
+        m,b = gradient_descent(m, b, data, alfa)
 
-print(m, b)
-plt.scatter(data.X, data.Y)
-plt.xlabel("chirps/sec for the stripped ground cricket")
-plt.ylabel("temperature (F)")
-plt.plot(list(range(10, 25)), [m * x + b for x in range(10,25)])
-plt.show()
+    print(m, b)
+    plt.scatter(data.X, data.Y)
+    plt.xlabel("chirps/sec for the stripped ground cricket")
+    plt.ylabel("temperature (F)")
+    plt.plot(list(range(10, 25)), [m * x + b for x in range(10,25)])
+    plt.show()
